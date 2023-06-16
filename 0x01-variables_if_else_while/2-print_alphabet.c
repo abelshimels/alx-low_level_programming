c1b3bbe3ceeed721751990994bb919dc176d0c26
abelshimels <abelshimels01@gmail.com>
2023-06-16 10:23:49 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
+ */
+int main(void)
+{
+	char chala = 'a';
+
+	while (chala <= 'z')
+	{
+		putchar(chala);
+		chala++;
+	}
+
+	putchar('\n');
+	return (0);
+}
